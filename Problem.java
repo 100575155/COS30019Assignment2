b87f1a664b78f3659a97c561335008ad167edaef
Bayley Papworth <bayley.w.papworth@gmail.com>
2017-05-05 20:10:09 +1000

    Added comments

--- Problem.java
+++ Problem.java
@@ -1,22 +1,25 @@
 import java.util.*;
 public class Problem {
 
-	private List<String> clauses;
+	private List<String> clauses;	//List of clauses
 	private String query;
 	
-	public Problem (String query) 
+	public Problem (String query) 			//NOTE: Query passed into the datastructure at construction
 	{
 		this.query = query;
 		clauses = new ArrayList<String>();
 	}
-	public void AddClause(String clause)
+	public void AddClause(String clause)		//Does what is says
 	{
 		clauses.add(clause);
 	}
-	public String getQuery() {
+	//Getters and setters
+	public String getQuery() 
+	{
 		return query;
 	}
-	public List<String> getClauses() {
+	public List<String> getClauses() 
+	{
 		return clauses;
 	}
 
