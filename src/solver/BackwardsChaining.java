41033452ba2d2d345ccf831b55e12fe197d240f0
Bayley Papworth <bayley.w.papworth@gmail.com>
2017-05-25 12:01:27 +1000

    Completed Backwards Chaining

--- src/solver/BackwardsChaining.java
+++ src/solver/BackwardsChaining.java
@@ -1,28 +1,35 @@
 package solver;
+
+import java.util.ArrayList;
+
 public class BackwardsChaining {
 
 	private Problem problem;
+	private ArrayList<String> returns;
 	public BackwardsChaining(Problem a){
 		problem = a;
+		returns = new ArrayList<String>();
 	}
-	public Boolean RunBackwardsChaining(){
-		boolean result;
-		for (int i =0; i< problem.assertions.size(); i++){						//checks if the query matches one of the assertions
+	public Boolean RunBackwardsChaining(String query){
+		
+		for (int i =0; i< problem.assertions.size(); i++){						//checks if the query matches an assertion
 			if (problem.query == problem.assertions.get(i).getOperand()){
 				System.out.println(problem.assertions.get(i).getOperand());
+				returns.add(problem.assertions.get(i).getOperand());
 				return true;
 			}
 		}
 		for (int i =0; i< problem.rules.size(); i++){							//checks if query is not the result of a rule
-			if (problem.query != problem.rules.get(i).getRightOperand()){
-				return false;
-			}else{
-				
+			if (problem.query == problem.rules.get(i).getRightOperand()){
+				System.out.println(problem.rules.get(i));
+				RunBackwardsChaining(problem.rules.get(i).getLeftOperand());
 			}
 		}
 		return false;
 	}
 	private void PrintBackwardsChain(){
+
+			System.out.println(returns);
 		
 	}
 
