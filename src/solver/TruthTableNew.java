8542d3d716bc2fc471bc08e24340e3debca9426b
Bayley Papworth <bayley.w.papworth@gmail.com>
2017-05-25 18:03:06 +1000

    Added class and GetSymbols class

--- src/solver/TruthTableNew.java
+++ src/solver/TruthTableNew.java
@@ -1,1 +1,12 @@
 
+public class TruthTableNew{
+  private String makeFunction(String input){
+
+  }
+  private String[] getSymbols(String input){
+    String[] strArray =  input.split(); //Regex to split at "=>"+";"+"&"
+    Set<String> mySet = new HashSet<String>(Arrays.asList(strArray));
+    return mySet
+  }
+
+}
