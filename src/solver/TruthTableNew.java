0bd1f230759ac5a65950a53f22353569eeb5f964
Lachie-Hines <Lachiehines@hotmail.com>
2017-05-25 18:04:00 +1000

    Update TruthTableNew.java
    Adding both truth functions

--- src/solver/TruthTableNew.java
+++ src/solver/TruthTableNew.java
@@ -10,3 +10,31 @@ public class TruthTableNew{
   }
 
 }
+// Function when => found to change to If (example X => Y)
+// X if Y.... X = Y
+
+function EqIf (Boolean x, Boolean y) // takes either side of (if/=>) and work out boolean
+  if (y == true) && (x == true){
+    return true;
+  }
+  else if (y == false) && (x == true){
+    return false;
+  }
+  else if(y == true) && (x == false){
+    return true;
+  }
+  else {
+    return true;
+  }
+
+
+// Function to combine with AND/&&
+
+function ChAnd (Boolean x, Boolean y) // takes either side of (&&/and) and works out boolean
+  if (x = true) && (y == true){
+    return true
+  }
+  else {
+    return false;
+  }
+
