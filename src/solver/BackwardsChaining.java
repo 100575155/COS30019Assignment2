059cf58d1f4c853b9a342b73f9f157a935b7bdee
Bayley Papworth <bayley.w.papworth@gmail.com>
2017-05-26 13:56:00 +1000

    Added Tests

--- src/solver/BackwardsChaining.java
+++ src/solver/BackwardsChaining.java
@@ -11,22 +11,26 @@ public class BackwardsChaining {
 		returns = new ArrayList<String>();
 	}
 	public Boolean RunBackwardsChaining(String query){
-		query =  query.replaceAll("\\s+","");
+		//query =  query.replaceAll("\\s+","");
 		
 		for (int i =0; i< problem.assertions.size(); i++){						//checks if the query matches an assertion
 			
-			System.out.println(query);
-			System.out.println(problem.assertions.get(i).getOperand());
-			System.out.println(problem.query == problem.assertions.get(i).getOperand());
-			if (problem.query == problem.assertions.get(i).getOperand()){
+			//System.out.println(query);
+			//System.out.println(problem.assertions.get(i).getOperand());
+			//System.out.println(problem.query == problem.assertions.get(i).getOperand());
+			if (query.equals(problem.assertions.get(i).getOperand())){
 				System.out.println(problem.assertions.get(i).getOperand());
 				returns.add(problem.assertions.get(i).getOperand());
 				return true;
 			}
 		}
 		for (int i =0; i< problem.rules.size(); i++){							//checks if query is not the result of a rule
-			if (problem.query == problem.rules.get(i).getRightOperand()){
-				returns.add(problem.rules.get(i).getLeftOperand()+ problem.rules.get(i).getOperator()+ problem.rules.get(i).getRightOperand());
+			
+			if (query.equals(problem.rules.get(i).getRightOperand())){
+				returns.add(problem.rules.get(i).getLeftOperand());
+				
+				System.out.println("Right: " +problem.rules.get(i).getRightOperand());
+				System.out.println("Left: "+problem.rules.get(i).getLeftOperand());
 				RunBackwardsChaining(problem.rules.get(i).getLeftOperand());
 			}
 		}
